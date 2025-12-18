0c3c2389b0927eb3b80f037b2e3a1f22c90d5ac1
SH Woo <imdlsrks.mc@gmail.com>
2025-12-18 21:26:55 +0900

    🐛 fix: 에러

--- app-webflux-chat/src/main/java/com/socrates/app/webflux/chat/service/ChatService.java
+++ app-webflux-chat/src/main/java/com/socrates/app/webflux/chat/service/ChatService.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 @Slf4j
